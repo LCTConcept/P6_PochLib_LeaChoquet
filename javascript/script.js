ea84eed2e8660813f688e6f0e9ca700603fef98a
LCTConcept <l.m.choquet@gmail.com>
2024-09-14 16:09:13 +0200

    Step 2 fix+ : Error (error message for duplicates when loading the page) fixed + code refactored. Next step : design.

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,11 @@
+// Function executed when the window is fully loaded
 window.onload = function() {
+  initAddBookButton();
+  loadBooksFromSession();
+};
+
+// Initialize the "Add a Book" button
+function initAddBookButton() {
   const button = document.createElement('button');
   button.id = 'ajouterLivreBtn';
   button.textContent = 'Ajouter un livre';
@@ -10,189 +17,184 @@ window.onload = function() {
 
   button.addEventListener('click', () => {
     if (!document.getElementById('search-container')) {
-      const searchContainer = document.createElement('div');
-      searchContainer.id = 'search-container';
-      searchContainer.classList.add('search-container');
-      
-      const form = document.createElement('form');
-      form.id = 'search-form';
-      form.classList.add('form-search');
-
-      const titleLabel = document.createElement('label');
-      titleLabel.htmlFor = 'book-title';
-      titleLabel.textContent = 'Titre du livre';
-      titleLabel.classList.add('label-title');
-
-      const titleInput = document.createElement('input');
-      titleInput.type = 'text';
-      titleInput.id = 'book-title';
-      titleInput.placeholder = 'Titre du livre';
-      titleInput.classList.add('input-title');
-
-      const authorLabel = document.createElement('label');
-      authorLabel.htmlFor = 'book-author';
-      authorLabel.textContent = 'Auteur';
-      authorLabel.classList.add('label-author');
-
-      const authorInput = document.createElement('input');
-      authorInput.type = 'text';
-      authorInput.id = 'book-author';
-      authorInput.placeholder = 'Auteur';
-      authorInput.classList.add('input-author');
-
-      const searchButton = document.createElement('button');
-      searchButton.type = 'submit';
-      searchButton.id = 'search-button';
-      searchButton.textContent = 'Rechercher';
-      searchButton.classList.add('btn-search');
-
-      const cancelButton = document.createElement('button');
-      cancelButton.type = 'button';
-      cancelButton.id = 'cancel-button';
-      cancelButton.textContent = 'Annuler';
-      cancelButton.classList.add('btn-cancel');
-
-      form.appendChild(titleLabel);
-      form.appendChild(titleInput);
-      form.appendChild(authorLabel);
-      form.appendChild(authorInput);
-      form.appendChild(searchButton);
-      form.appendChild(cancelButton);
-
-      searchContainer.appendChild(form);
-      myBooksDiv.insertBefore(searchContainer, separator);
-
-      form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const title = titleInput.value.trim();
-        const author = authorInput.value.trim();
-        if (title && author) {
-          performSearch(title, author);
-        } else {
-          alert('Both fields are required.');
-        }
-      });
-
-      cancelButton.addEventListener('click', () => {
-        searchContainer.remove();
-      });
+      createSearchForm(myBooksDiv, separator);
     }
   });
-};
-
-function generateBookId(title, author) {
-  return `${title}-${author}`.replace(/\s+/g, '-').toLowerCase();
 }
 
-function performSearch(title, author) {
-  const searchContainer = document.getElementById('search-container');
+// Create and display the search form
+function createSearchForm(container, separator) {
+  const searchContainer = document.createElement('div');
+  searchContainer.id = 'search-container';
+  searchContainer.classList.add('search-container');
+  
+  const form = document.createElement('form');
+  form.id = 'search-form';
+  form.classList.add('form-search');
 
+  form.innerHTML = `
+    <label for="book-title" class="label-title">Titre du livre</label>
+    <input type="text" id="book-title" placeholder="Titre du livre" class="input-title">
+    <label for="book-author" class="label-author">Auteur</label>
+    <input type="text" id="book-author" placeholder="Auteur" class="input-author">
+    <button type="submit" id="search-button" class="btn-search">Rechercher</button>
+    <button type="button" id="cancel-button" class="btn-cancel">Annuler</button>
+  `;
+
+  searchContainer.appendChild(form);
+  container.insertBefore(searchContainer, separator);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = form.querySelector('#book-title').value.trim();
+    const author = form.querySelector('#book-author').value.trim();
+    if (title && author) {
+      performSearch(title, author);
+    } else {
+      alert('Ces deux champs sont obligatoires.');
+    }
+  });
+
+  document.getElementById('cancel-button').addEventListener('click', () => {
+    searchContainer.remove();
+  });
+}
+
+// Search for a book using the Google Books API
+function performSearch(title, author) {
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}`)
     .then(response => response.json())
-    .then(data => {
-      const resultsDiv = document.createElement('div');
-      resultsDiv.id = 'search-results';
-      resultsDiv.classList.add('search-results');
-
-      if (data.totalItems === 0) {
-        resultsDiv.textContent = "Aucun livre n'a été trouvé.";
-      } else {
-        data.items.forEach(item => {
-          const book = item.volumeInfo;
-          const bookId = generateBookId(book.title, book.authors ? book.authors[0] : 'Auteur inconnu');
-          const bookDiv = document.createElement('div');
-          bookDiv.classList.add('book-item');
-
-          const titleElem = document.createElement('h3');
-          titleElem.textContent = book.title;
-          titleElem.textContent = `Titre : ${book.title || 'Titre non disponible'}`;
-          bookDiv.appendChild(titleElem);
-
-          const idElem = document.createElement('p');
-          idElem.textContent = `ID: ${bookId}`;
-          bookDiv.appendChild(idElem);
-
-          const authorElem = document.createElement('p');
-          authorElem.textContent = `Auteur : ${book.authors ? book.authors[0] : 'Auteur inconnu'}`;
-          bookDiv.appendChild(authorElem);
-
-          const descriptionElem = document.createElement('p');
-          descriptionElem.textContent = `Description : ${book.description ? book.description.substring(0, 200) + '...' : 'Information manquante'}`;
-          bookDiv.appendChild(descriptionElem);
-
-          const img = document.createElement('img');
-          img.src = book.imageLinks ? book.imageLinks.thumbnail : 'img/unavailable.png';
-          img.alt = 'Image du livre';
-          bookDiv.appendChild(img);
-
-          // Create bookmark icon
-          const bookmarkIcon = document.createElement('i');
-          bookmarkIcon.classList.add('fa-regular', 'fa-bookmark');
-          bookmarkIcon.style.color = '#74C0FC';
-          bookmarkIcon.addEventListener('click', () => {
-            addToPochListe(book, bookId);
-          });
-          bookDiv.appendChild(bookmarkIcon);
-
-          resultsDiv.appendChild(bookDiv);
-        });
-      }
-      searchContainer.appendChild(resultsDiv);
-    });
+    .then(data => displaySearchResults(data))
+    .catch(error => console.error('Error fetching search results:', error));
 }
 
-// Add the book to the "poch'liste"
-function addToPochListe(book, bookId) {
-  const pochListeDiv = document.getElementById('poch-liste');
-  if (!pochListeDiv) {
-    const pochListe = document.createElement('div');
-    pochListe.id = 'poch-liste';
-    pochListe.classList.add('poch-liste');
-    document.body.appendChild(pochListe);
+// Display search results
+function displaySearchResults(data) {
+  const searchContainer = document.getElementById('search-container');
+  const resultsDiv = document.createElement('div');
+  resultsDiv.id = 'search-results';
+  resultsDiv.classList.add('search-results');
+
+  if (data.totalItems === 0) {
+    resultsDiv.textContent = "Aucun livre n'a été trouvé.";
+  } else {
+    data.items.forEach(item => createBookItem(item, resultsDiv));
   }
 
-  // Check if book is already in the list
-  if (document.getElementById(bookId)) {
-    alert('Vous ne pouvez ajouter deux fois le même livre.');
+  searchContainer.appendChild(resultsDiv);
+}
+
+// Create a book item element and add it to the results
+function createBookItem(item, container) {
+  const book = item.volumeInfo;
+  const bookId = item.id;
+
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book-item');
+
+  bookDiv.innerHTML = `
+    <h3>Titre : ${book.title || 'Titre non disponible'}</h3>
+    <p>ID: ${bookId}</p>
+    <p>Auteur : ${book.authors ? book.authors[0] : 'Auteur inconnu'}</p>
+    <p>Description : ${book.description ? book.description.substring(0, 200) + '...' : 'Information manquante'}</p>
+    <img src="${book.imageLinks ? book.imageLinks.thumbnail : 'img/unavailable.png'}" alt="Image du livre">
+    <i class="fa-regular fa-bookmark" style="color: #74C0FC"></i>
+  `;
+
+  bookDiv.querySelector('.fa-bookmark').addEventListener('click', () => {
+    addToPochListe(book, bookId);
+  });
+
+  container.appendChild(bookDiv);
+}
+
+// Add a book to the Poch'List
+function addToPochListe(book, bookId, checkDuplicate = true) {
+  if (checkDuplicate && isBookInPochList(bookId)) {
+    alert('Ce livre est déjà dans votre poch\'liste.');
     return;
   }
 
+  const pochListeDiv = document.getElementById('poch-liste') || createPochListeDiv();
+  const bookItem = createBookItemDiv(book, bookId);
+
+  pochListeDiv.appendChild(bookItem);
+  saveBookToSession(book, bookId);
+}
+
+// Check if a book is already in the Poch'List
+function isBookInPochList(bookId) {
+  const currentBooks = JSON.parse(sessionStorage.getItem('pochListe')) || [];
+  return currentBooks.some(b => b.id === bookId);
+}
+
+// Create a book item element for the Poch'List
+function createBookItemDiv(book, bookId) {
   const bookItem = document.createElement('div');
   bookItem.id = bookId;
   bookItem.classList.add('poch-book-item');
 
-  const titleElem = document.createElement('h3');
-  titleElem.textContent = `Titre : ${book.title || 'Titre non disponible'}`;
-  bookItem.appendChild(titleElem);
+  bookItem.innerHTML = `
+    <h3>Titre : ${book.title || 'Titre non disponible'}</h3>
+    <p>ID: ${bookId}</p>
+    <p>Auteur : ${book.authors ? book.authors[0] : 'Auteur inconnu'}</p>
+    <p>Description : ${book.description ? book.description.substring(0, 200) + '...' : 'Information manquante'}</p>
+    <img src="${book.imageLinks ? book.imageLinks.thumbnail : 'img/unavailable.png'}" alt="Image du livre">
+    <i class="fa-solid fa-trash" style="color: #FF6B6B"></i>
+  `;
 
-
-  const idElem = document.createElement('p');
-  idElem.textContent = `ID: ${bookId}`;
-  bookItem.appendChild(idElem);
-
-  const author = document.createElement('p');
-  author.textContent = `Auteur : ${book.authors ? book.authors[0] : 'Auteur inconnu'}`
-  bookItem.appendChild(author);
-
-  const descriptionElem = document.createElement('p');
-  descriptionElem.textContent = `Description : ${book.description ? book.description.substring(0, 200) + '...' : 'Information manquante'}`;
-  bookItem.appendChild(descriptionElem);
-
-
-  const img = document.createElement('img');
-  img.src = book.imageLinks ? book.imageLinks.thumbnail : 'img/unavailable.png';
-  img.alt = 'Image du livre';
-  bookItem.appendChild(img);
-
-  // Create trash icon
-  const trashIcon = document.createElement('i');
-  trashIcon.classList.add('fa-solid', 'fa-trash');
-  trashIcon.style.color = '#FF6B6B';
-  trashIcon.addEventListener('click', () => {
+  bookItem.querySelector('.fa-trash').addEventListener('click', () => {
     bookItem.remove();
+    removeBookFromSession(bookId);
   });
-  bookItem.appendChild(trashIcon);
 
-  const pochListe = document.getElementById('poch-liste');
-  pochListe.appendChild(bookItem);
+  return bookItem;
+}
+
+// Create the Poch'List div if it doesn't exist yet
+function createPochListeDiv() {
+  const pochListe = document.createElement('div');
+  pochListe.id = 'poch-liste';
+  pochListe.classList.add('poch-liste');
+  document.body.appendChild(pochListe);
+  return pochListe;
+}
+
+// Save a book to sessionStorage
+function saveBookToSession(book, bookId) {
+  const currentBooks = JSON.parse(sessionStorage.getItem('pochListe')) || [];
+  if (!isBookInPochList(bookId)) {
+    const newBook = {
+      id: bookId,
+      title: book.title,
+      author: book.authors ? book.authors[0] : 'Auteur inconnu',
+      description: book.description || '',
+      image: book.imageLinks ? book.imageLinks.thumbnail : 'img/unavailable.png'
+    };
+    currentBooks.push(newBook);
+    sessionStorage.setItem('pochListe', JSON.stringify(currentBooks));
+  }
+}
+
+// Load books saved in sessionStorage
+function loadBooksFromSession() {
+  const savedBooks = JSON.parse(sessionStorage.getItem('pochListe')) || [];
+  const pochListeDiv = document.getElementById('poch-liste') || createPochListeDiv();
+
+  savedBooks.forEach(book => {
+    const bookData = {
+      title: book.title,
+      authors: [book.author],
+      description: book.description,
+      imageLinks: { thumbnail: book.image }
+    };
+    addToPochListe(bookData, book.id, false);
+  });
+}
+
+// Remove a book from sessionStorage
+function removeBookFromSession(bookId) {
+  const currentBooks = JSON.parse(sessionStorage.getItem('pochListe')) || [];
+  const updatedBooks = currentBooks.filter(book => book.id !== bookId);
+  sessionStorage.setItem('pochListe', JSON.stringify(updatedBooks));
 }
